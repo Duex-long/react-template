d44813454a6a11e56e4808df9d76267ed2bbcbe0
duex <duex0702@gmail.com>
2024-09-10 17:44:10 +0800

    feat: 新增ant

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,32 @@
+import { useMemo, useState } from 'react'
 import './index.less'
 
+const Render_text = ({ val }: { val: boolean }) => {
+
+  return <div>Render-Text</div>
+}
+
+const Render_btn = () => {
+  console.log('render')
+  const val = false
+  const [count, setCount] = useState(1)
+  const addCount = () => setCount(count + 1)
+  const MemoRender = useMemo(() => Render_text, [])
+  return (
+    <>
+      <MemoRender val={val} />
+      <button onClick={addCount}> Click</button>
+    </>
+  )
+}
+
 const Home = () => {
-  return <div className="app-home"></div>
+  return (
+    <div className="app-home">
+      HOME
+      <Render_btn />
+    </div>
+  )
 }
 
 export default Home
